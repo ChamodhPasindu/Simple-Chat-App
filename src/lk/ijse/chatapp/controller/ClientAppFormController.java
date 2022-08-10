9e1e0dcc8c1dd8b05670dcb18a22793601321484
ChamodhPasindu <chamodhpasindu2000@gmail.com>
2022-08-10 12:26:20 +0530

    make few changes in fxml files

--- src/lk/ijse/chatapp/controller/ClientAppFormController.java
+++ src/lk/ijse/chatapp/controller/ClientAppFormController.java
@@ -1,4 +1,35 @@
 package lk.ijse.chatapp.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ClientAppFormController {
+    public TextField txtMessageArea;
+    public Pane mainContext;
+    public Pane messageAreaContext;
+    public Text txtUsername;
+
+    public void initialize(){
+    }
+
+
+    public void logoutBtnOnAction(ActionEvent actionEvent) throws IOException {
+        Stage window= (Stage) mainContext.getScene().getWindow();
+        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/clientLoginForm.fxml"))));
+    }
+
+    public void sendMessageOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void sendPhotoOnAction(ActionEvent actionEvent) {
+
+    }
 }
--- src/lk/ijse/chatapp/controller/ClientLoginFormController.java
+++ src/lk/ijse/chatapp/controller/ClientLoginFormController.java
@@ -2,6 +2,7 @@ package lk.ijse.chatapp.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -16,10 +17,18 @@ public class ClientLoginFormController {
     public void loginBtnOnAction(ActionEvent actionEvent) throws IOException {
         String username=txtUsername.getText();
 
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../view/clientAppForm.fxml"));
+        Parent parent=loader.load();
+        ClientAppFormController controller = loader.<ClientAppFormController>getController();
+        controller.txtUsername.setText(username);
+        Stage window= (Stage) loginContext.getScene().getWindow();
+        window.setScene(new Scene(parent));
+/*
         switch (username) {
             case "chamodh":
-                Stage window= (Stage) loginContext.getScene().getWindow();
-                window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/clientAppForm.fxml"))));
+                parent parent=FXMLLoader.load(getClass().getResource("")
+
+
                 break;
             case "pasindu":
                 System.out.println("pasindu");
@@ -30,6 +39,6 @@ public class ClientLoginFormController {
             default:
                 System.out.println("wrong");
                 break;
-        }
+        }*/
     }
 }
