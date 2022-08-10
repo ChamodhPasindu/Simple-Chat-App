a15a5fe3ca1495a52a37ce7e5171f848f91afc57
ChamodhPasindu <chamodhpasindu2000@gmail.com>
2022-08-10 09:33:40 +0530

    create login and application interfaces

---
+++ src/lk/ijse/chatapp/controller/ClientAppFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.chatapp.controller;
+
+public class ClientAppFormController {
+}
--- src/lk/ijse/chatapp/controller/ClientLoginFormController.java
+++ src/lk/ijse/chatapp/controller/ClientLoginFormController.java
@@ -1,4 +1,35 @@
 package lk.ijse.chatapp.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ClientLoginFormController {
+    public TextField txtUsername;
+    public AnchorPane loginContext;
+
+    public void loginBtnOnAction(ActionEvent actionEvent) throws IOException {
+        String username=txtUsername.getText();
+
+        switch (username) {
+            case "chamodh":
+                Stage window= (Stage) loginContext.getScene().getWindow();
+                window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/clientAppForm.fxml"))));
+                break;
+            case "pasindu":
+                System.out.println("pasindu");
+                break;
+            case "sahan":
+                System.out.println("sahan");
+                break;
+            default:
+                System.out.println("wrong");
+                break;
+        }
+    }
 }
